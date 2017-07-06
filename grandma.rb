ac8368672db219f633dcdb4c050582dd51313b1a
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-07-06 12:15:11 -0400

    grandma

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,15 @@
-# Write a speak_to_grandma method.
 
+
+def speak_to_grandma(speak)
+    if speak == speak.upcase && speak != "I LOVE YOU GRANDMA!"
+        "NO, NOT SINCE 1938!"
+        elsif speak == "I LOVE YOU GRANDMA!"
+            "I LOVE YOU TOO PUMPKIN!"
+        else
+        "HUH?! SPEAK UP, SONNY!"
+    end
+end
+    
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
